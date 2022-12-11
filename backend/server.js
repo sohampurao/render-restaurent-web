5c80f649f9789538f28d4bf2bcf852ec47a6c87d
Soham Purao <sohampurao1@gamil.com>
2022-12-12 01:53:18 +0530

    fix update

--- backend/server.js
+++ backend/server.js
@@ -60,10 +60,6 @@ app.get('/api/config/paypal', (req, res) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
-
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
